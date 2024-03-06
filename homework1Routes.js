d7d492a099ab0418bdade83b54492c6affeb1829
Emma Chen <emmachen@starlux-airlines.com>
2024-03-06 16:31:49 +0800

    add package.json

--- homework1Routes.js
+++ homework1Routes.js
@@ -1,8 +1,5 @@
-const fs = require('fs');
-
 const requestHandler = (req, res)=> {
     const url = req.url;
-    const method = req.method;
 
     if(url === '/') {
         res.write('<!DOCTYPE html>');
